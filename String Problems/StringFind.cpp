cc203a202938cd9287ff7cee888d6a0d48f402c9
freakfan15 <vivekkrrai.bxr@gmail.com>
2021-05-31 13:52:15 +0530

    Find index of String in paragraph

---
+++ String Problems/StringFind.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main(){
+
+	string paragraph = "We are learning about STL strings. STL string class is quite powerful";
+
+	string word;
+	getline(cin,word);
+
+	//find function
+	int index =  paragraph.find(word);
+
+	if(index!=-1){
+		cout <<"first occ = " <<index<<endl;
+	}
+
+	index = paragraph.find(word,index+1);
+
+	if(index!=-1){
+		cout<<"second occ = "<<index <<endl;
+	}
+
+	if(index==-1){
+		cout<<"String not found"<<endl;
+	}
+
+
+
+	return 0;
+}
