d6aca0ce6d24779cbb5076e87762b8c6142d1e3b
freakfan15 <vivekkrrai.bxr@gmail.com>
2022-01-20 15:34:17 +0530

    Added search in ll

--- Linked List/creatingLinkedList.cpp
+++ Linked List/creatingLinkedList.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 
 class Node {
-	int data;
+	
 public:
+	int data;
 	Node* next;
 
 	Node(int d):data(d), next(NULL){}
@@ -25,7 +26,7 @@ public:
 	}
 
 	void push_front(int data){
-		if(head==NULL){
+		if(head==NULL){	
 			Node* n = new Node(data);
 			head = tail = n;
 		}
@@ -47,6 +48,37 @@ public:
 			tail = n;
 		}
 	}
+
+	void inserAtMiddle(int data, int pos){
+		if(pos==0){
+			push_front(data);
+		}
+		else{
+			Node* temp = head;
+			//take temp to pos-1
+			for(int jump=1; jump<=pos-1; jump++){
+				temp = temp->next;
+			}
+
+			Node* n = new Node(data);
+			n->next = temp->next;
+			temp->next = n;
+		}
+	}
+
+	int search(int key){
+		Node * temp = head;
+
+		int idx = 0;
+		while(temp!=NULL){
+			if(temp->data == key){
+				return idx;
+			}
+			idx++;
+			temp = temp->next;
+		}
+		return -1;
+	}
 };
 
 
@@ -56,14 +88,24 @@ int main(int argc, char const *argv[])
 	List l;
 	l.push_back(1);
 	l.push_back(0);
-	l.push_back(2);
+	l.push_back(3);
+	l.push_back(4);
+
+	l.inserAtMiddle(2, 2);
 
 	Node* head = l.begin();
+
+	cout<<"The elements in ll are: ";
 
 	while(head!=NULL){
 		cout<<head->getData()<<" ";
 		head = head->next;
 	}
+
+	cout<<endl<<"Enter the element you want to search: ";
+	int key;
+	cin>>key;
+	cout<<l.search(key)<<endl;
 	
 	return 0;
 }
